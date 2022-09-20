4ae5f161b2d5e14c1fecf6568c81606e6421e04e
Carlos Pinedo <carlos.pinedo@devo.com>
2022-09-20 19:24:06 +0200

    Revert "ci: Changed Pages deploy method"
    This reverts commit aa9fc38733951c16f83b7d770728895c11b8a538.

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+// THIS FILE IS AUTO GENERATED
+import { IconType } from './IconBase.esm.js';
+export declare const DiCheck: IconType;
+export declare const DiDelete: IconType;
+export declare const DiMoreOptions: IconType;
+export declare const DiRefresh: IconType;
+export declare const DiSave: IconType;
