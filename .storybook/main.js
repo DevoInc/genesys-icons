5246f7b3d7e20b155d246eff1a1d0079ad045cad
Horacio Gouveia <horacio.gouveia@devo.com>
2022-10-24 17:48:48 +0200

    ci: added webpack 5 for storybook, compatible with node18

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
   framework: '@storybook/react',
   core: {
+    builder: 'webpack5',
     disableTelemetry: true,
   },
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
   ],
 };
