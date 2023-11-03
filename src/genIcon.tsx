fa6f945c5e9e860f86b154dc6d60c9ce7a867505
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-11-03 16:46:36 +0100

    chore: fix Icon cmp displayName

--- src/genIcon.tsx
+++ src/genIcon.tsx
@@ -4,9 +4,14 @@ import { IconBase, IconBaseProps } from './IconBase';
 import { IconTree } from './declarations';
 import { Tree2Element } from './Tree2Element';
 
-export const genIcon =
-  (data: IconTree, tags: string) => (props: IconBaseProps) => (
+export const genIcon = (data: IconTree, tags: string) => {
+  const Icon = (props: IconBaseProps) => (
     <IconBase attr={{ ...data.attrs }} data-tags={tags} {...props}>
       {Tree2Element(data.children)}
     </IconBase>
   );
+  // Find 'title' children and get its text
+  const iconName = data.children.find((child) => child.tag === 'title')?.text;
+  Icon.displayName = `gi-${iconName}`;
+  return Icon;
+};
