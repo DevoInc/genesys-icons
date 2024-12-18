43f1ae79ecb9ae05e5e387c6e0714a9129d1861f
Javier López Úbeda <jlopezcur@gmail.com>
2024-12-18 16:22:46 +0100

    feat: added react 19 compatibility

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  //minify: 'terser',
+  format: ['esm', 'cjs'],
+  metafile: true,
+  treeshake: true,
+  external: ['react'],
+});
