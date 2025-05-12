8ed60f828509ffa4c9c3cbdf3e02a91f090b0367
Javier López Úbeda <jlopezcur@gmail.com>
2025-05-12 16:09:06 +0200

    test: fix message when invalid characters on icon name

--- scripts/validations/iconName/validations/checkInvalidCharacters.ts
+++ scripts/validations/iconName/validations/checkInvalidCharacters.ts
@@ -1,7 +1,7 @@
 const re = /^[a-z_][a-z_0-9]*$/;
 
 export const reasonInvalidCharacter =
-  'Invalid character, only lower case and numbers are allowed';
+  'Invalid character, only lower case, numbers (but not at the start), and underscores are allowed';
 
 export const checkInvalidCharacter = (name: string) =>
   re.test(name)
