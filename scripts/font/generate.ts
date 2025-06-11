3e33071a7d34cc8e3cf7a36feb4acd615dffed4a
Horacio Gouveia <horacio.gouveia@devo.com>
2025-06-11 16:35:28 +0200

    fix: renaming font family for all style files

--- scripts/font/generate.ts
+++ scripts/font/generate.ts
@@ -40,49 +40,59 @@ svgtofont({
     // We needed to pass extra css props somehow.
     fontSize: `inherit;  ${strProps}`,
   },
-  svgicons2svgfont: {
-    fontHeight: 1000,
-    normalize: true,
-  },
+  svgicons2svgfont: { fontHeight: 1000, normalize: true },
   classNamePrefix: 'gi',
-}).then(() => {
-  /* eslint-disable-next-line no-console */
-  console.log('Font: Generated icons fonts!');
+})
+  .then(() => {
+    /* eslint-disable-next-line no-console */
+    console.log('Font: Generated icons fonts!');
 
-  const stylesFile = resolve(pkgPath, `${config.fontName}-styles.scss`);
-  const allStyles = fs.readFileSync(stylesFile).toString();
-  const [fontFace, classGeneric, classes, variables] = allStyles.split('\n\n');
+    const stylesFile = resolve(pkgPath, `${config.fontName}-styles.scss`);
+    const allStyles = fs.readFileSync(stylesFile).toString();
+    const [fontFace, classGeneric, classes, variables] =
+      allStyles.split('\n\n');
 
-  // Writting the SCSS variables file
-  const variablesFile = resolve(pkgPath, `${config.fontName}-variables.scss`);
-  fs.writeFileSync(variablesFile, variables, { encoding: 'utf8' });
+    // Writting the SCSS variables file
+    const variablesFile = resolve(pkgPath, `${config.fontName}-variables.scss`);
+    fs.writeFileSync(variablesFile, variables, { encoding: 'utf8' });
 
-  // Replace the font-family since svgicons2svgfont.fontName (that is suppose to
-  // do this) is not working
-  const fontFaceReplaced = fontFace.replace(
-    /font-family: "(.*)"/,
-    `font-family: "gi"`,
-  );
-  const classGenericReplaced = classGeneric.replace(
-    /font-family: '(.*)'/,
-    `font-family: 'gi'`,
-  );
+    // Rewrite styles file with classes referencing to SCSS variables
+    fs.writeFileSync(
+      stylesFile,
+      `${fontFace}\n\n${classGeneric}\n\n${getEditedIconClasses(classes)}`,
+      { encoding: 'utf8' },
+    );
 
-  // Rewrite styles file with classes referencing to SCSS variables
-  fs.writeFileSync(
-    stylesFile,
-    `${fontFaceReplaced}\n\n${classGenericReplaced}\n\n${getEditedIconClasses(
-      classes,
-    )}`,
-    { encoding: 'utf8' },
-  );
-
-  // Writting base SCSS file with the imports of both variables and styles
-  const baseFile = resolve(pkgPath, `${config.fontName}.scss`);
-  const baseContent = `// VARIABLES
+    // Writting base SCSS file with the imports of both variables and styles
+    const baseFile = resolve(pkgPath, `${config.fontName}.scss`);
+    const baseContent = `// VARIABLES
 @import '${config.fontName}-variables';
 
 // ${config.fontName} FONT STYLES
 @import '${config.fontName}-styles';`;
-  fs.writeFileSync(baseFile, baseContent, { encoding: 'utf8' });
-});
+    fs.writeFileSync(baseFile, baseContent, { encoding: 'utf8' });
+  })
+  .then(() => {
+    console.log('Font: Renaming font-family');
+
+    ['css', 'scss', 'less', 'module.less', 'styl'].forEach((ext) => {
+      const file = resolve(pkgPath, `${config.fontName}-styles.${ext}`);
+      const allStyles = fs.readFileSync(file).toString();
+      const [fontFace, classGeneric, classes] = allStyles.split('\n\n');
+
+      const fontFaceReplaced = fontFace.replace(
+        /font-family: "(.*)"/,
+        `font-family: "gi"`,
+      );
+      const classGenericReplaced = classGeneric.replace(
+        /font-family: '(.*)'/,
+        `font-family: 'gi'`,
+      );
+
+      fs.writeFileSync(
+        file,
+        `${fontFaceReplaced}\n\n${classGenericReplaced}\n\n${getEditedIconClasses(classes)}`,
+        { encoding: 'utf8' },
+      );
+    });
+  });
